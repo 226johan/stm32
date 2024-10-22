5fc896b474974d84370f691786507fb072091078
226johan <2806762759@qq.com>
2024-10-22 15:51:10 +0800

    key_led_onoff

---
+++ key_led_onoff_register/Objects/key.d
@@ -0,0 +1,8 @@
+.\objects\key.o: Hardware\src\KEY\key.c
+.\objects\key.o: .\Hardware\include\KEY\key.h
+.\objects\key.o: .\Start\stm32f10x.h
+.\objects\key.o: .\Start\core_cm3.h
+.\objects\key.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\key.o: .\Start\system_stm32f10x.h
+.\objects\key.o: .\User\delay.h
+.\objects\key.o: .\Hardware\include\LED\led.h
